8921e241e957a643cf5a9feae220e529df6d9ea6
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2019-11-07 11:53:10 +0100

    Update ConvertArrayofObjectInObject.js

--- ConvertArrayofObjectInObject.js
+++ ConvertArrayofObjectInObject.js
@@ -5,15 +5,18 @@ var returnObject = convertArrayInObject([
   {'Super Test' : '2937823'} // to have a key with a space, use the quotation marks
   ]);
 
-  console.log(returnObject);
+  console.log(returnObject); // {Romain: '1234', Test: '12761', 'Super Test' : '2937823'}
+  ]);
   // console.log(returnObject.Romain);
   // console.log(returnObject['Super Test'])
 
 function convertArrayInObject(data){
-  var result = {} // init object
-  for (var i in data){
-    // result = Object.assign(result, data[i]); // Object method 
-    result[Object.keys(data[i])] = Object.values(data[i])[0]; // without method
+ var result = {} // init object
+  for (var i = 0; i < data.length ; i++){
+    //    // result = Object.assign(result, data[i]); // Object method 
+    var key = Object.keys(data[i])[0]; 
+    var value = data[i][Object.keys(data[i])[0]];
+    result[key] = value; // without method
   }
   return result;
 }
