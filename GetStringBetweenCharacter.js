6e0ce16f284588d2050e832a16a70f2752a965be
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2019-11-22 10:27:37 +0100

    Create GetStringBetweenCharacter.js

---
+++ GetStringBetweenCharacter.js
@@ -0,0 +1,14 @@
+/*** get string between character - ex : getStringBetweenCharacter("Test [1234]", "[", "]") = "1234"
+@param(string) string to split (this is mail subject)
+@param(string) fist character before the string to get
+@param(string) second character affter the string to get
+@return(string) return string between charac1 and charac2
+***/
+console.log(getStrCharac("Test [1234]", "[", "]")); // return '1234'
+
+function getStringBetweenCharacter(str, charac1, charac2){
+  return str.substring(
+    str.lastIndexOf(charac1) + 1, 
+    str.lastIndexOf(charac2)
+  );
+}
