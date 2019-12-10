325c8f4cbfb9abcbcd5c35a829c0410eadb42dd0
Romain Roux <cuisinier.sonore@gmail.com>
2019-12-10 09:25:20 +0100

    update code with filter

--- FindValueArrayObjects.js
+++ FindValueArrayObjects.js
@@ -1,16 +1,14 @@
 /*** Function to find value in Array of objects ***/
 
-  var result = findValueInArrayObjects([
-    {'dossier1' : 'Hello world'},
-    {'dossier2' : 'Good Bye'}
-    ], 
-    'dossier2'); // find value of dossier2
-  console.log(result); // 'Good Bye'
+var test = findValueInArrayObjects(
+  [{ dossier1: 'Hello world' }, { dossier2: 'Good Bye' }],
+  'dossier2' // find value of dossier2
+);
+console.log(test); // 'Good Bye'
 
-function findValueInArrayObjects(data, find){
-  for (var i in data){
-    if (data[i][find] !== undefined){ // if the text contains spaces
-      return data[i][find];
-    }
-  }
+function findValueInArrayObjects(data, find) {
+  var res = data.filter(function(x) {
+    return x[find] !== undefined;
+  });
+  return res.length === 0 ? false : res[0][find];
 }
