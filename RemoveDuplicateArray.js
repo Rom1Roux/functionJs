1e9a399b65fdc075d030a675e2fb30b7ab688b67
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2019-12-23 11:47:47 +0100

    comments update

--- RemoveDuplicateArray.js
+++ RemoveDuplicateArray.js
@@ -1,7 +1,11 @@
 /**
-* Function to remove duplicates from an array
-* @return{array} return array
+* Function to remove duplicates elements from an array
+* @param{array} array that be contains string, number or bolean (can be work with objects)
+* @return{array} return array without duplicates elements
 */
 function removeDuplicate(arr){
   return arr.filter(function(item,index){ return arr.indexOf(item) === index});
 }
+
+var tab = ['test', 'test', 'success', 'test']
+console.log(removeDuplicate(tab)); // return ["test","success"]
