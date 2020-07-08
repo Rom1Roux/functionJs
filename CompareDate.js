8ea1b20e6501e42c1efed21dbde030abc0e1faad
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2020-07-08 17:17:21 +0200

    add function buildArrDates

--- CompareDate.js
+++ CompareDate.js
@@ -44,3 +44,22 @@ function isSameHours(date1, date2) {
   // console.log(Date.parse(res[0]) === Date.parse(res[1]))
   return Date.parse(res[0]) === Date.parse(res[1]);
 }
+
+
+/**
+* Make an array with incremented dates
+* Starts on today's date and ends according to the number of days in parameter
+* @param{number} nbrDays is the total number of days
+* @return{array} return an array with the days incremented
+*/
+function buildArrDates(nbrDays) {
+  let dt = new Date();
+  let arrDate = [new Date()];
+
+  for (let i = 0; i < nbrDays - 1; i++) {
+    const dateInc = dt.setDate(dt.getDate() + 1);
+    arrDate.push(new Date(dateInc));
+  }
+
+  return arrDate;
+}
