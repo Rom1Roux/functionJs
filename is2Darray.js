19415abca43319b28887fe948ade7a5aff6d4c80
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2020-11-12 15:32:52 +0100

    Create is2Darray.js

---
+++ is2Darray.js
@@ -0,0 +1,18 @@
+/**
+* This function test if it's a 2D array
+*
+* param {Array} arr must an array or 2D array
+* return {Boolean} return boolean
+*/
+function is2DArr(arr){
+  return arr.filter(x=> typeof x === 'object').length === arr.length;
+}
+
+const arr1 = [[1, 2, 3],[1, 2, 3, "string"]];
+console.log(is2DArr(arr1)) // return true
+
+const arr2 = ["string", [1, 2, 3]];
+console.log(is2DArr(arr2)) // return false
+
+const arr3 = ["string1", "string2"];
+console.log(is2DArr(arr3)) // return false
