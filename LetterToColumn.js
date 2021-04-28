91505b21a4a891911ab7376bd2d5d54bb5b743e4
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-04-28 18:46:57 +0200

    Create LetterToColumn.js

---
+++ LetterToColumn.js
@@ -0,0 +1,18 @@
+const columnToLetter = (column) => {
+  let temp, letter = '';
+  while (column > 0) {
+    temp = (column - 1) % 26;
+    letter = String.fromCharCode(temp + 65) + letter;
+    column = (column - temp - 1) / 26;
+  }
+  return letter;
+}
+
+const letterToColumn = (letter) => {
+  letter = letter.toUpperCase();
+  let column = 0, length = letter.length;
+  for (let i = 0; i < length; i++) {
+    column += (letter.charCodeAt(i) - 64) * Math.pow(26, length - i - 1);
+  }
+  return column;
+}
