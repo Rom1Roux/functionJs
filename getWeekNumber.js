42fe9c9fc6b268758d6cc47aea1a4898b7806e34
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-04-28 14:14:49 +0200

    Create getWeekNumber.js

---
+++ getWeekNumber.js
@@ -0,0 +1,18 @@
+/* 
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ */
+function getWeekNumber(d) {
+  d = new Date(d);
+
+  // Get first day of year
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  // Calculate full weeks to nearest Thursday
+  var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+  // Return array of year and week number
+  return weekNo;
+}
