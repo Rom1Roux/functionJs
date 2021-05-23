06e8f3b64c30539f243835814ada2894619bd83d
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 17:30:26 +0200

    Update SortArrayOfArrays.js

--- SortArrayOfArrays.js
+++ SortArrayOfArrays.js
@@ -1,25 +1,15 @@
-var data = [
-  ["", 23, "Actif"],
-  ["row2", 23, ""],
-  ["row3",1, "b-Vérification en cours"], 
-  ["",1, "Actif"]
-]
-console.log(sortArrayOfArrays(data, 2)) // return  ["row2", 23, ""], ["", 23, "Actif"], ["",1, "Actif"],   ["row3",1, "b-Vérification en cours"], 
-
-
 /**
-* Function to sort an array of arrays on a column
-* @param{array} this array of arrays
-* @param{number} number correpond to column number
-* @return{array} return array of arrays sorted
-*/
-function sortArrayOfArrays(tab, column){
-  tab.sort(function(a, b) {
-    
+ * Function to sort an array of arrays on a column
+ * @param {Array} this array of arrays
+ * @param {Number} number correpond to column number
+ * @return {Array} return array of arrays sorted
+ */
+const sortArrayOfArrays = (tab, column) =>
+  tab.sort(function (a, b) {
     // si current value and next value is'nt empty or undefined then change value to uppercase else empty cell
-    var elCurrent = a[column] ? a[column].toUpperCase() : "" ; 
-    var elNext    = b[column] ? b[column].toUpperCase() : "" ;
-    
+    const elCurrent = a[column] ? a[column].toUpperCase() : "";
+    const elNext = b[column] ? b[column].toUpperCase() : "";
+
     if (elCurrent < elNext) {
       return -1;
     }
@@ -29,5 +19,11 @@ function sortArrayOfArrays(tab, column){
     // names must be equal
     return 0;
   });
-  return tab;
-}
+
+const data = [
+  ["", 23, "Actif"],
+  ["row2", 23, ""],
+  ["row3", 1, "b-Vérification en cours"],
+  ["", 1, "Actif"]
+];
+console.log(sortArrayOfArrays(data, 2)); // return  ["row2", 23, ""], ["", 23, "Actif"], ["",1, "Actif"],   ["row3",1, "b-Vérification en cours"],
