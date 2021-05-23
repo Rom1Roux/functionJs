c00a53e9213ebde9a9352a23fb18763d55197494
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 16:39:48 +0200

    version ES6

--- ConvertArrayofObjectInObject.js
+++ ConvertArrayofObjectInObject.js
@@ -1,22 +1,27 @@
+/**
+ * Version ES6
+ * Convert Array of objects (possible with multiple keys) in one object
+ * @param {Array} data is an Array of Objects
+ * @returns {Object}
+ */
+const convertArrayInObject = (data) => {
+  let obj = {}; // init object
+  data.forEach((el) =>
+    Object.values(el).forEach((value, i) => {
+      const key = Object.keys(el)[i];
+      obj = { ...obj, [key]: value };
+    })
+  );
+  return obj;
+};
+
 // Convert Array of objects in a sigle Object
-var returnObject = convertArrayInObject([
-  {Romain: '1234'},
-  {Test: '12761'},
-  {'Super Test' : '2937823'} // to have a key with a space, use the quotation marks
-  ]);
+const arr = [
+  { Romain: "1234" },
+  { Test: "12761" },
+  { "Super Test": "2937823" } // to have a key with a space, use the quotation marks
+];
 
-  console.log(returnObject); // {Romain: '1234', Test: '12761', 'Super Test' : '2937823'}
-  ]);
-  // console.log(returnObject.Romain);
-  // console.log(returnObject['Super Test'])
-
-function convertArrayInObject(data){
- var result = {} // init object
-  for (var i = 0; i < data.length ; i++){
-    //    // result = Object.assign(result, data[i]); // Object method 
-    var key = Object.keys(data[i])[0]; 
-    var value = data[i][Object.keys(data[i])[0]];
-    result[key] = value; // without method
-  }
-  return result;
-}
+console.log(convertArrayInObject(arr)); // {Romain: '1234', Test: '12761', 'Super Test' : '2937823'}]);
+// console.log(returnObject.Romain);
+// console.log(returnObject['Super Test'])
