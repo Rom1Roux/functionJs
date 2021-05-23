490f64491eb4d93348a3f5ef36a1a57302062f49
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 17:26:44 +0200

    new version

--- SortArrOfObj.js
+++ SortArrOfObj.js
@@ -1,15 +1,18 @@
-const arrObj = [{ name: "Romain", order: 2 }, { name: "Etienne", order: 6 }, { name: "Jean-Paul", order: 1 }];
-console.log(sortArrOfObj(arrObj));
-
 /**
  * sort an array of objects follow the order key
  * @param {Array} arr is an array of objects
  * @return {Array} return Array sorted
  */
-function sortArrOfObj(arr) {
-  return arr.sort((a, b) => {
+const sortArrOfObj = (arr) =>
+  arr.sort((a, b) => {
     let comparison = 0;
     a.order > b.order ? (comparison = 1) : (comparison = -1);
     return comparison;
   });
-}
+
+const arrObj = [
+  { name: "Romain", order: 2 },
+  { name: "Etienne", order: 6 },
+  { name: "Jean-Paul", order: 1 }
+];
+console.log(sortArrOfObj(arrObj));
