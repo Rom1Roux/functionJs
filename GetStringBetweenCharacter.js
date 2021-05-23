a3b96cbe88977f9bf52ecc5056e3d3376a58a278
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 16:51:47 +0200

    version ES6

--- GetStringBetweenCharacter.js
+++ GetStringBetweenCharacter.js
@@ -1,14 +1,14 @@
-/*** get string between character - ex : getStringBetweenCharacter("Test [1234]", "[", "]") = "1234"
-@param(string) string to split (this is mail subject)
-@param(string) fist character before the string to get
-@param(string) second character affter the string to get
-@return(string) return string between charac1 and charac2
-***/
-console.log(getStringBetweenCharacter("Test [1234]", "[", "]")); // return '1234'
+/**
+ * Version ES6
+ * get string between character
+ * ex : getStringBetweenCharacter("Test [1234]", "[", "]") = "1234"
+ *
+ * @param {String} string to split (this is mail subject)
+ * @param {String} fist character before the string to get
+ * @param {String} second character affter the string to get
+ * @return {String} return string between charac1 and charac2
+ ***/
+const getStringBetweenCharacter = (str, charac1, charac2) =>
+  str.substring(str.lastIndexOf(charac1) + 1, str.lastIndexOf(charac2));
 
-function getStringBetweenCharacter(str, charac1, charac2){
-  return str.substring(
-    str.lastIndexOf(charac1) + 1, 
-    str.lastIndexOf(charac2)
-  );
-}
+console.log(getStringBetweenCharacter("Test [1234]", "[", "]")); // return '1234'
