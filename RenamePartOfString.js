ae76e88c8dffa86ceed338405fda41c187357f6c
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 17:24:58 +0200

    Update RenamePartOfString.js

--- RenamePartOfString.js
+++ RenamePartOfString.js
@@ -1,7 +1,17 @@
-/*** Function to rename a part of a string of characters between brackets ***/
-
-function newTitle (old, string){
-  return old.split('[')[0] + ' [' + string + ']';
+/**
+ * Rename a part of a string of characters between brackets
+ * @param {String} old is old string to replaced
+ * @param {String} string to replace
+ */
+function newTitle(old, string) {
+  return old.split("[")[0] + " [" + string + "]";
 }
 
-console.log(newTitle('test [2019]', 2020)); // 'test  [2020]'
+const str = "test [2019]";
+const newString = "2020";
+console.log(newTitle(str, newString)); // 'test  [2020]'
+
+
+// Use with replace
+const res = str.replace("2019", "2020");
+console.log(res);
