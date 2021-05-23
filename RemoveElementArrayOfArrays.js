154d0c91d32bf624ac5d954c20b94d690acfd534
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 17:16:44 +0200

    add version ES6

--- RemoveElementArrayOfArrays.js
+++ RemoveElementArrayOfArrays.js
@@ -1,27 +1,16 @@
-// if equal then update, if only in master then #N/A, if only in extract add row
-var headers = ["header1", "", "headers3", ""];
-var data = [
-  ["row1 Column1", "row1 Column2", "row1 Column3", "row1 Column4"],
-  ["row2 Column1", "row2 Column2", "row2 Column3", "row2 Column4"]
-];
-
-// return Object {headers: ["header1", "headers3"], data : [["row1 Column1", "row1 Column3"],["row2 Column1", "row2 Column3"]];
-console.log(removeColumnEmpty(headers, data)); 
-
-
 /**
  * Function to remove column if element empty into header
- * @param{Array} Array that contain strings (header)
- * @param{Array} Array that contain arrays (data spread sheet)
- * @return{object} return obeject with header and data updated
+ * @param {Array} Array that contain strings (header)
+ * @param {Array} Array that contain arrays (data spread sheet)
+ * @return {object} return obeject with header and data updated
  */
 function removeColumnEmpty(headers, data) {
   // create an array with index of element header empty
   var emptyList = headers
-    .map(function(el, i) {
+    .map(function (el, i) {
       return (el === "" || el === " " || el === undefined) && i;
     })
-    .filter(function(el) {
+    .filter(function (el) {
       return el !== false;
     });
 
@@ -50,3 +39,36 @@ function removeColumnEmpty(headers, data) {
   }
   return { headers: newHeaders, data: newData };
 }
+
+/**
+ * ES6 version
+ * Function to remove column if element empty into header
+ * @param {Array} Array that contain strings (header)
+ * @param {Array} Array that contain arrays (data spread sheet)
+ * @return {object} return obeject with header and data updated
+ */
+const removeColumnEmpty2 = (headers, data) => {
+  const indexToRemove = [];
+  headers = headers.filter((el, i) => {
+    if (el === "" || el === undefined) {
+      indexToRemove.push(i);
+      return false;
+    }
+    return true;
+  });
+  data = data.map((row) =>
+    row.filter((col, i) => indexToRemove.indexOf(i) === -1)
+  );
+  return { headers, data };
+};
+
+// if equal then update, if only in master then #N/A, if only in extract add row
+var headers = ["header1", "", "headers3", ""];
+var data = [
+  ["row1 Column1", "row1 Column2", "row1 Column3", "row1 Column4"],
+  ["row2 Column1", "row2 Column2", "row2 Column3", "row2 Column4"]
+];
+
+// return Object {headers: ["header1", "headers3"], data : [["row1 Column1", "row1 Column3"],["row2 Column1", "row2 Column3"]];
+console.log("ES5", removeColumnEmpty(headers, data));
+console.log("ES6", removeColumnEmpty2(headers, data));
