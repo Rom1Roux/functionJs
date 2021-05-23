e7e87ad6850b6a587a99cc88014659ff038e1adc
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2021-05-23 16:47:34 +0200

    version ES6

--- FindValueArrayObjects.js
+++ FindValueArrayObjects.js
@@ -1,14 +1,26 @@
-/*** Function to find value in Array of objects ***/
-
-var test = findValueInArrayObjects(
-  [{ dossier1: 'Hello world' }, { dossier2: 'Good Bye' }],
-  'dossier2' // find value of dossier2
-);
-console.log(test); // 'Good Bye'
-
+/**
+ * Find value in an Array of Objects
+ * @param {Array} data is an Aray of objects
+ * @param {String} find is key to get value
+ */
 function findValueInArrayObjects(data, find) {
-  var res = data.filter(function(x) {
+  var res = data.filter(function (x) {
     return x[find] !== undefined;
   });
   return res.length === 0 ? false : res[0][find];
 }
+
+/**
+ * ES6 version
+ * Find value in an Array of Objects
+ * @param {Array} data is an Aray of objects
+ * @param {String} find is key to get value
+ */
+const findValueInArrayObjects2 = (data, find) =>
+  data.filter((el) => el[find])[0][find];
+
+const arr = [{ dossier1: "Hello world" }, { dossier2: "Good Bye" }];
+const key = "dossier2";
+
+console.log(findValueInArrayObjects(arr, key)); // 'Good Bye'
+console.log(findValueInArrayObjects2(arr, key)); // 'Good Bye'
