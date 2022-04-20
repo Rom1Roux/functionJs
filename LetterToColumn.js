8e5ca8c14e6120c203c5608451ebfcfaec086ba6
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2022-04-20 11:42:50 +0200

    add function rangeSheetToNumericJs

--- LetterToColumn.js
+++ LetterToColumn.js
@@ -16,3 +16,22 @@ const letterToColumn = (letter) => {
   }
   return column;
 }
+
+
+/**
+ * convert single range (ex: A2) to understandable numeric value for javascript loops
+ * Example with A2 : column A becomes number 0, row 2 becomes number 1
+ * @param {String} range
+ * @returns {Object} contains column and row number
+ */
+const rangeSheetToNumericJs = (range) => {
+  const row = parseInt(range.match(/\d+/g)[0]);
+  let letter = range.match(/[a-zA-Z]+/g)[0];
+
+  letter = letter.toUpperCase();
+  let column = 0, length = letter.length;
+  for (let i = 0; i < length; i++) {
+    column += (letter.charCodeAt(i) - 64) * Math.pow(26, length - i - 1);
+  }
+  return { column: column - 1, row: row - 1 };
+}
