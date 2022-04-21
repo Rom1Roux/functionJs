3c8686b9ab434f50f0a2c3b13e69edf4d368f9da
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2022-04-21 18:21:39 +0200

    Update ConcatArrayofArrays.js

--- ConcatArrayofArrays.js
+++ ConcatArrayofArrays.js
@@ -1,6 +1,19 @@
+/**
+ * Version ES5 
+ * Transform Array 2D into simple Array
+ * @param {Array) arr
+ */
 function concatArrays(arr){
   return [].concat.apply([], arr);
 }
 
-var tab = [['tab1'],['tab2'],['tab3'],['tab4']]
-console.log(concatArrays(tab));
+/**
+ * Version ES6 
+ * Transform Array 2D into simple Array
+ * @param {Array) arr
+ */
+const concatArrays_2 = (arr)=> arr.flat();
+
+const tab = [['tab1'],['tab2'],['tab3'],['tab4']]
+console.log(concatArrays(tab)); // return ['tab1','tab2','tab3','tab4']
+console.log(concatArrays_2(tab)); // return ['tab1','tab2','tab3','tab4']
