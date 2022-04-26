295e389e1e2c68823392572e0f5b32f33b0c37cb
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2022-04-26 11:14:16 +0200

    Update RemoveDuplicateArray.js

--- RemoveDuplicateArray.js
+++ RemoveDuplicateArray.js
@@ -16,5 +16,13 @@ function removeDuplicate(arr){
 const removeDuplicate2 = (arr) =>
   arr.filter((item, index) => arr.indexOf(item) === index);
 
+/**
+ * ES6 with Set method
+ * A Set is a collection of unique values. To remove duplicates from an array
+ * @param {Array} arr that be contains string, number or bolean (can be work with objects)
+ * @return {Array} return array without duplicates elements
+ */
+const removeDupplicate3 = (arr) => [...new Set(arr)];
+
 const tab = ["test", "test", "success", "test"];
 console.log(removeDuplicate(tab)); // return ["test","success"]
